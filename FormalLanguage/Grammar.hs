bc90c6ef2128a2cb3d6516e96521c539148c80b3
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2013-10-22 01:05:15 +0200

    updated lenses, and enumeration

--- FormalLanguage/Grammar.hs
+++ FormalLanguage/Grammar.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -21,38 +22,84 @@ import           Data.Set (Set)
 import qualified Data.Set as S
 import           Data.Foldable
 import           Control.Applicative
+import qualified Control.Lens.Indexed as Lens
 
 
+
+-- | Grammar indices are enumerable objects
+--
+-- TODO should we always assume operations "modulo"?
+
+data Enumerable
+  = Singular
+  | IntBased Integer [Integer]
+  | Enumerated String [String]
+  deriving (Eq,Ord,Show)
+
+instance Default Enumerable where
+  def = Singular
 
 -- | A single-dimensional terminal or non-terminal symbol.
 
 data Symb where
-  T  :: String -> Symb
-  NT :: String -> Symb
+  T  :: String               -> Symb
+  NT :: String -> Enumerable -> Symb
 
 deriving instance Show Symb
+deriving instance Eq   Symb
 
 symb :: Lens' Symb String
-symb f (T  s) = T  <$> f s
-symb f (NT s) = NT <$> f s
+symb f (T  s  ) = T                <$> f s
+symb f (NT s e) = (\s' -> NT s' e) <$> f s
 
--- | A complete grammatical symbol is multi-dimensional (with 0 .. )
--- dimensions.
+enumed :: Lens' Symb Enumerable
+enumed f (T  _  ) = error "enumed doesn't exist for terminal symbols"
+enumed f (NT s e) = NT s <$> f e
+
+-- | A complete grammatical symbol is multi-dimensional with 0..  dimensions.
 
 newtype Symbol = Symbol { getSymbs :: [Symb] }
 
 deriving instance Show Symbol
+deriving instance Eq   Symbol
 
 symbol :: Lens' Symbol [Symb]
 symbol f (Symbol xs) = Symbol <$> f xs  -- are we sure?
 
+type instance Index Symbol = Int
+
 type instance IxValue Symbol = Symb
 
-instance At Symbol where
-  at = undefined
-
 instance Applicative f => Ixed f Symbol where
-  ix k f (Symbol xs) = undefined -- Symbol <$> ix k f xs
+  ix k f (Symbol xs) = Symbol <$> ix k f xs
+  {- -- more manual version of "ix"
+  ix k f (Symbol xs) | k<0 = pure (Symbol xs)
+                     | otherwise = go xs k
+                     where go []     _ = pure (Symbol [])
+                           go (y:ys) 0 = Lens.indexed f k y <&> (\y' -> Symbol $ y':ys)
+                           go (y:ys) l = (\(Symbol ys') -> Symbol $ y : ys') <$> (go ys $! l - 1)
+                           -}
+
+-- |
+
+data Rule = Rule
+  { _lhs :: Symbol
+  , _fun :: ()
+  , _rhs :: [Symbol]
+  } deriving (Eq,Show)
+
+makeLenses ''Rule
+
+-- |
+
+data Grammar = Grammar
+  { _tsyms       :: Set Symbol
+  , _nsyms       :: Set Symbol
+  , _productions :: Set Rule
+  , _start       :: Symbol
+  } deriving (Show)
+
+makeLenses ''Grammar
 
 {-
 
