5f67b02d127ce2bdc22a8a01320c246f53212ef0
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2013-12-01 01:27:18 +0100

    working on pretty printer

--- GrammarPP.hs
+++ GrammarPP.hs
@@ -1,13 +1,30 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 
 -- | This small utility allows us to turn a formal language description into
 -- either a LaTeX source file or a Haskell module.
 
 module Main where
 
+import System.Console.CmdArgs
+
 
 
 data Options
-  = LaTeX { select :: Maybe String    -- Select one of the grammars to print (or all)
-          }
+  = LaTeX
+    {
+    }
+  | Ansi
+    {
+    }
+  deriving (Show,Data,Typeable)
 
-main = return ()
+optionLatex = LaTeX
+  {
+  }
+
+optionAnsi = Ansi
+
+main = do
+  o <- cmdArgs $ modes [optionLatex,optionAnsi]
+  print o
+  return undefined
