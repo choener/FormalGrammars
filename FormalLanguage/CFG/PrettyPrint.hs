799810d799bc974f89447a8a0e8555d7fe1a123a
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2015-04-21 11:35:29 +0200

    some cleanup

--- FormalLanguage/CFG/PrettyPrint.hs
+++ FormalLanguage/CFG/PrettyPrint.hs
@@ -1,7 +1,11 @@
 
 module FormalLanguage.CFG.PrettyPrint
-  ( renderLaTeX
+  ( module FormalLanguage.CFG.PrettyPrint.ANSI
+  , module FormalLanguage.CFG.PrettyPrint.Haskell
+  , module FormalLanguage.CFG.PrettyPrint.LaTeX
   ) where
 
-import FormalLanguage.CFG.PrettyPrint.LaTeX as LaTeX
+import FormalLanguage.CFG.PrettyPrint.ANSI
+import FormalLanguage.CFG.PrettyPrint.Haskell
+import FormalLanguage.CFG.PrettyPrint.LaTeX
 
