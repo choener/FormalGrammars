1891d11a16e314987560d40861e1996dac05705e
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-10-01 11:12:29 +0200

    disabled latex output for now
    latex output should be done nicely and new

--- FormalLanguage/CFG/PrettyPrint.hs
+++ FormalLanguage/CFG/PrettyPrint.hs
@@ -2,10 +2,10 @@
 module FormalLanguage.CFG.PrettyPrint
   ( module FormalLanguage.CFG.PrettyPrint.ANSI
   , module FormalLanguage.CFG.PrettyPrint.Haskell
-  , module FormalLanguage.CFG.PrettyPrint.LaTeX
+--  , module FormalLanguage.CFG.PrettyPrint.LaTeX
   ) where
 
 import FormalLanguage.CFG.PrettyPrint.ANSI
 import FormalLanguage.CFG.PrettyPrint.Haskell
-import FormalLanguage.CFG.PrettyPrint.LaTeX
+--import FormalLanguage.CFG.PrettyPrint.LaTeX
 
