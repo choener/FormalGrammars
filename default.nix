37486fc2dd5022889587a14f8d43b2ebda1dd400
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-10-02 10:28:39 +0200

    fixed up FormalGrammars for travis release

--- default.nix
+++ default.nix
@@ -1,26 +1,21 @@
 { mkDerivation, ADPfusion, ansi-wl-pprint, base, bytestring
-, cmdargs, containers, data-default, HaTeX, lens, mtl, parsers
-, PrimitiveArray, QuickCheck, semigroups, smallcheck, stdenv, tasty
-, tasty-quickcheck, tasty-smallcheck, tasty-th, template-haskell
-, text, transformers, trifecta, unordered-containers, vector
+, containers, data-default, lens, mtl, parsers, PrimitiveArray
+, semigroups, stdenv, template-haskell, text, transformers
+, trifecta, unordered-containers, vector
 }:
 mkDerivation {
   pname = "FormalGrammars";
-  version = "0.3.1.1";
+  version = "0.4.0.0";
   src = ./.;
-  isLibrary = true;
-  isExecutable = true;
   libraryHaskellDepends = [
     ADPfusion ansi-wl-pprint base bytestring containers data-default
-    HaTeX lens mtl parsers PrimitiveArray semigroups template-haskell
-    text transformers trifecta unordered-containers vector
-  ];
-  executableHaskellDepends = [
-    ansi-wl-pprint base cmdargs trifecta
+    lens mtl parsers PrimitiveArray semigroups template-haskell text
+    transformers trifecta unordered-containers vector
   ];
   testHaskellDepends = [
-    base QuickCheck smallcheck tasty tasty-quickcheck tasty-smallcheck
-    tasty-th
+    ADPfusion ansi-wl-pprint base bytestring containers data-default
+    lens mtl parsers PrimitiveArray semigroups template-haskell text
+    transformers trifecta unordered-containers vector
   ];
   homepage = "https://github.com/choener/FormalGrammars";
   description = "(Context-free) grammars in formal language theory";
